3936b80d2fbeb6424792a54b18e2bf521dd39252
coconutkim <achaeyeon18@gmail.com>
2024-07-11 12:31:50 +0900

    Add files via upload

---
+++ myHours.py
@@ -0,0 +1,11 @@
+# my codes
+ttime = input("주어진 시간:")
+study = input("하루 공부 시간:")
+ttime = int(ttime)
+study = int(study)
+
+a = ttime//study//365
+b = ttime//study%365
+c = ttime%study
+
+print("{0}년 {1}일 {2}시간동안 화이팅하세요.".format(a, b, c))
