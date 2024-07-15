3f2f05b125b73038b69df1698e8db8f10a9696cd
coconutkim <achaeyeon18@gmail.com>
2024-07-15 17:45:33 +0900

    Add files via upload

---
+++ hi2.py
@@ -0,0 +1,2 @@
+print("hi2")
+print("hihihi")
---
+++ test2.py
@@ -0,0 +1,10 @@
+print("hello")
+
+print('Hello Python')
+# Hello Python
+
+print("hello!"*3)
+# hello!hello!hello!
+
+print('혼자 공부하다 모르면 동영상 강의를 참고하세요!')
+# 혼자 공부하다 모르면 동영상 강의를 참고하세요!
