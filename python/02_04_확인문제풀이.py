20d79e1cdcf97b3ff399954010be83f1bd92ded8
coconutkim <achaeyeon18@gmail.com>
2024-07-22 01:23:17 +0900

    1

--- python/02_04_확인문제풀이.py
+++ python/02_04_확인문제풀이.py
@@ -1,19 +1,19 @@
-a = input("> 1번째 숫자: ")
-b = input("> 2번째 숫자: ")
-print()
+# a = input("> 1번째 숫자: ")
+# b = input("> 2번째 숫자: ")
+# print()
 
-print("{} + {} = {}" .format(a, b, int(a)+int(b)))
-
-
+# print("{} + {} = {}" .format(a, b, int(a)+int(b)))
 
 
 
-# string = "hello"
 
-# string.upper()
-# print("A 지점:", string)
 
-# print("B 지점:", string.upper())
+string = "hello"
+
+string.upper()
+print("A 지점:", string)
+
+print("B 지점:", string.upper())
 
 
 
