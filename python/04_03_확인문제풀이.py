aec604905be2365c6001d066e1603905d4af6e03
coconutkim <achaeyeon18@gmail.com>
2024-07-23 11:14:28 +0900

    1

--- python/04_03_확인문제풀이.py
+++ python/04_03_확인문제풀이.py
@@ -24,28 +24,28 @@ i = 1
 
 
 # sum_value = 0
-# while sum_value <limit:
+while sum_value <limit:
    
-#     sum_value              +=i
-#     i +=1  
+    sum_value              +=i
+    i +=1  
 
 
 
-# print(f'{i}를 더할 때 {limit}을 넘으며 그때의 값은 {sum_value}입니다.')
+print(f'{i}를 더할 때 {limit}을 넘으며 그때의 값은 {sum_value}입니다.')
 
-# ------------------------------------------------------
+------------------------------------------------------
 
-# max_value = 0
-# a = 0
-# b = 0
+max_value = 0
+a = 0
+b = 0
 
-# for i in range(1,100):
-#     j = 100-i
+for i in range(1,100):
+    j = 100-i
 
 
-#     if max_value<i*j:
-#         max_value = i*j
-#         a=i
-#         b=j
+    if max_value<i*j:
+        max_value = i*j
+        a=i
+        b=j
 
-# print('최대가 되는 경우 {} * {} = {}'.format(a,b,max_value))
+print('최대가 되는 경우 {} * {} = {}'.format(a,b,max_value))
