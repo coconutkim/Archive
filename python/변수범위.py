054c601089e0bfc4148ea604d350613846dc8f0b
coconutkim <achaeyeon18@gmail.com>
2024-07-23 12:31:22 +0900

    1

---
+++ python/변수범위.py
@@ -0,0 +1,12 @@
+i = 5
+
+print(i)
+print(id(i))
+print("==============")
+for i in range(3):
+    print(id(i))
+    # print(i)
+
+print('=============================')
+# 막판에 있는 i의 주소
+print(id(i))
