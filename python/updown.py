01597490063c31f297a272004ae90be48909a85a
coconutkim <achaeyeon18@gmail.com>
2024-07-25 11:21:10 +0900

    1

--- python/updown.py
+++ python/updown.py
@@ -1,14 +1,37 @@
+# 틀린 답
 import random
-a = input('number:')
-a = int(a)
+num = int(input('number:'))
 
-if a in range(1,101):
+if num in range(1,101):
     a = random.randint(1,100)
-    if a > 37:
-        print('down')
-    elif a < 37:
-        print('up')
+    if num != a:
+        if num > a:
+            print('down')
+        elif num < a:
+            print('up')   
     else:
         print('bingo')
 else:
     print('wrong number')
+   
+    
+    
+    
+print('---------------------------------')
+
+# 정답
+import random
+secret_num =random.randint(1,100)
+print('answer:', secret_num)
+i = 1
+while i<=5:
+    guess_num = int(input('number:'))
+    if secret_num > guess_num:
+        print('up')
+    elif secret_num < guess_num:
+        print('down')
+    else:
+        print('bingo')
+        break
+    i = i+1
+print('the end')
