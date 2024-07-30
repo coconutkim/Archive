37178f6285bd1bbad4f23e0d64a463a88a586e36
coconutkim <achaeyeon18@gmail.com>
2024-07-30 18:26:09 +0900

    1

--- js/jsex1.js
+++ js/jsex1.js
@@ -1,6 +1,12 @@
-'hi';
-console.log('hi2');
-console.log('hi55');
+
+// console.log('hi2');
+// console.log('hi55');
+
+alert('watch out')
+window.alert('watch out')
+prompt('당신의 나이는?');
+
+
 
 /*
-console.log('hi');
+console.log('hi')*/
