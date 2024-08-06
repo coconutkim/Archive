52bb515c58f39abac56dcc7d6331b4091193ad08
coconutkim <achaeyeon18@gmail.com>
2024-08-07 01:17:35 +0900

    1

---
+++ js/확인문제/확인문제_5장.js
@@ -0,0 +1,14 @@
+// function test(a,b,c){
+//     console.log(a);
+//     console.log(a*b);
+//     console.log(a*b*c);
+// }
+
+// test(10,100)
+
+
+function multiply(args) {
+    return args.multiply(())
+};
+
+multiply()
